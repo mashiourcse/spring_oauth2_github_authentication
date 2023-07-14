f24bbb732eecf895ee22a46e20ce76f5d96d94ce
mashiourcse <mashiour.cseiiuc@gmail.com>
2023-07-14 20:13:39 +0600

    user endpoint is not accessable

---
+++ src/main/java/com/codemash/config/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package com.codemash.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests(authConfig -> {
+				authConfig.requestMatchers(HttpMethod.GET, "/").permitAll();
+				authConfig.requestMatchers(HttpMethod.GET, "/user").hasRole("USER");
+				authConfig.requestMatchers(HttpMethod.GET, "/user").hasAnyAuthority("ROLE_USER", "OIDC_USER");
+				authConfig.requestMatchers(HttpMethod.GET, "/admin").hasRole("ADMIN");
+				authConfig.anyRequest().authenticated();
+			})
+			.csrf(csrf -> csrf.disable())
+			.headers().frameOptions().disable()
+			.and()
+			.formLogin(withDefaults()) // Login with browser and Build in Form
+			.httpBasic(withDefaults()) // Login with Insomnia or Postman and Basic Auth       
+			.oauth2Login(withDefaults())// Login with Google - GitHub - Facebook or .......
+			;
+			return http.build();
+	}
+		
+	@Bean
+	JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+		return jdbcUserDetailsManager;
+	}
+	
+	@Bean
+    DataSource getDataSource() {
+		return DataSourceBuilder.create()
+				.driverClassName("org.h2.Driver")
+				.url("jdbc:h2:mem:testdb")
+				.username("sa")
+				.password("")
+				.build();
+    }	
+}
